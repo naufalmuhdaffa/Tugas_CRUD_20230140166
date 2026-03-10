2116a1019dc195368d3ebe8454efa585002f8387
naufalmuhdaffa <naufaldaffa878@gmail.com>
2026-03-10 20:31:11 +0700

    chore: init

---
+++ src/test/java/com/deploy/ktp/KtpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deploy.ktp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
