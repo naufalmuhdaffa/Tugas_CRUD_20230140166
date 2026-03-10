133986b3e68369b78bd97bd211fe95d7cc9638b1
naufalmuhdaffa <naufaldaffa878@gmail.com>
2026-03-10 20:44:22 +0700

    feat: add ktp repository

---
+++ src/main/java/com/deploy/ktp/repository/KtpRepository.java
@@ -0,0 +1,12 @@
+package com.deploy.ktp.repository;
+
+import com.deploy.ktp.model.entity.Ktp;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface KtpRepository extends JpaRepository<Ktp, Integer> {
+
+    Optional<Ktp> findByNomorKtp(String nomorKtp);
+
+}
